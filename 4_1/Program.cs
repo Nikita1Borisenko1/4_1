e0d7934ddc98364ba274b15f1b6bd1926e432a97
ZKDS <nikitoc0@mail.ru>
2024-03-30 09:47:47 +0300

    Добавьте файлы проекта.

---
+++ 4_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace example_4_01;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Write("Введите количество строк: ");
+        int row = int.Parse(ReadLine());
+        Write("Введите количество столбцов: ");
+        int col = int.Parse(ReadLine());
+
+        int[,] matrix = new int[row, col];
+
+        Random random = new Random();
+
+        int s = 0;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                matrix[i, j] = random.Next(10);
+                s += matrix[i, j];
+                Write($"{matrix[i, j]}");
+            }
+           
+           WriteLine();
+
+        }
+        WriteLine($"\nсумма всех элементов матрицы равна = {s}");
+    }
+}
+
